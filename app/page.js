ac16c57bfc986debebea617fb1696b7656a3d3a3
Naveen gupta <gjagarnath54@gmail.com>
2025-06-11 16:40:01 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -32,7 +32,7 @@ const Page = () => {
       } else {
         clearInterval(interval);
       }
-    },[sentences], 80); // adjust typing speed here
+    },[sentences], 150); // adjust typing speed here
     return () => clearInterval(interval);
   }, []);
 
